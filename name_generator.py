9e863d44a41c7ca9e23676c96201ece1904a5204
MWiechmann <mail@mwiechmann.com>
2021-11-26 21:48:56 +0100

    switched from jupyter notebook to script file; added distribution exe-file

---
+++ name_generator.py
@@ -0,0 +1,192 @@
+# SETTINGS (will later be read from .ini file)
+# Lists with name parts (based on GERMAN FL publications - so far core set and Raven's Purge has been added)
+human_male_p1 = ["Bryn", "Har", "Oro", "Pa", "Ver", "Von", "Hol", "Al", "Al", "Zy", "Al", "Hin", "Na", "Har", "Ro", "Ga",
+                 "Oke", "Ar", "Ser", "Ver", "Ul", "El", "Ya", "Stur", "Per", "Ne", "Ol", "Vi", "Bei", "Ne", "Tol", "Es",
+                 "Dal", "Kor", "Fer", "Kal", "Mor", "Har", "Ha", "Hra", "Mo", "Al", "Kar", "Un", "Kra", "Mahin",
+                "Ma", "Ha", "Bran", "Ga", "Ser", "Au", "Pa", "Skun", "Gut", "A", "Jor", "Va", "An", "Ra", "Ro", "Rem",
+                "Di", "Es", "Pun", "Ka", "Man", "Kar"]
+human_male_p2 = ["", "", "", "sin", "ge", "", "ga", "va", "go", "ga", "", "", "", "", "mar", "", "",
+                 "ge", "", "", "", "", "po", "", "", "", "", "do", "re", "", "ra", "", "", "", "", "", "", "", "", "",
+                 "ran", "ga", "tor", "tul", "ni", "", "me", "", "", "", "", "la", "", "", "", "", "", "", "", "bre", "",
+                 "ta", "", "rup", "der", "du"]
+human_male_p3 = ["de", "dar", "vald", "der", "vil", "mar", "mar", "gard", "fer", "rod", "rek", "trix", "wa", "bert",
+                 "gol", "ren", "told", "vil", "rand", "wim", "kas", "ko", "la", "ke", "dor", "me", "gar", "mar",
+                 "bald", "max", "ma", "r", "ss", "m", "b", "m", "m", "s", "b", "ng", "der", "red", "da", "dar",
+                "los", "ver", "t", "das", "d", "v", "val", "g", "kus", "sa", "harm", "gur", "g", "lom", "selm", "vil", "bund", "do",
+                "mir", "ga", "zo", "tus", "el", "ne"]
+
+human_fem_p1 = ["Gar", "Ha", "Nir", "Sin", "Ta", "Tera", "Tve", "La", "Ca", "Eng",
+                "Lin", "Gin", "Ane", "O", "Al", "Poll", "Nir", "Ur", "Grem", "Dyn", "Lan", "Vi", "Ri", "Vi", "Mi",
+               "Har", "Ar", "Vul", "Za", "Mi", "Sky", "Dar", "Lu", "Tar", "Mor", "Vir", "Vi", "Bre"]
+human_fem_p2 = ["mel", "lin", "me", "", "le", "mal", "", "vi",
+                "tya", "", "na", "", "", "", "", "", "su", "", "", "", "sa", "", "mel", "rel", "se", "", "",
+               "", "ge", "ri", "", "", "", "", "dar", "", "", "zi"]
+human_fem_p3 = ["da", "de", "na", "dia", "ne", "da", "dra", "de", "la", "sle",
+                "ya", "da", "ta", "ma", "morr", "vea", "la", "la", "dria", "ja", "ja", "na", "da", "da", "la",
+               "gan", "bia", "m", "re", "dia", "lia", "ja", "mi", "na", "ga", "nia", "seria", "ga"]
+
+elf_male_p1 = ["Dor", "Ha", "Or", "Rad", "Ver", "Ne", "Al", "Kal", "Al", "Krit", "Hiz"]
+elf_male_p2 = ["ra", "go", "", "gel", "", "bu", "ga", "", "sur", "", "ge"]
+elf_male_p3 = ["krond", "dan", "nar", "lo", "m", "los", "red", "man", "so", "re", "le"]
+
+elf_fem_p1 = ["Blei", "Eri", "Kri", "Si", "Voro",
+              "Hel", "Mar", "Ge", "Ney", "Irid", "Vi", "Ul", "Klo", "Irid", "Kel", "Mar", "Me", "Ma"]
+elf_fem_p2 = ["nel", "", "blen", "me", "val", "der", "", "mel", "", "", "ri", "ma", "tin", "", "", "", "gon",
+              "li"]
+elf_fem_p3 = ["la", "mi", "da", "ne", "lia",
+              "min", "tea", "da", "d", "ne", "dia", "ya", "da", "", "dra", "te", "da", "na"]
+
+halfelf_male_p1 = ["Ga", "Ga", "Hor", "Jyr", "San", "Zer", "Vol"]
+halfelf_male_p2 = ["ver", "li", "der", "", "dar", "tor", ""]
+halfelf_male_p3 = ["in", "van", "an", "man", "me", "me", "ler"]
+
+halfelf_fem_p1 = ["Hi", "Ho", "Ko", "So", "Tri", "The", "Oral", "Mar"]
+halfelf_fem_p2 = ["ve", "ler", "re", "ni", "mel", "ra", "", ""]
+halfelf_fem_p3 = ["na", "va", "na", "lia", "lia", "nia", "da", "ga"]
+
+orc_male_p1 = ["Hay", "Alach", "Ar", "Gol-Gar",
+               "Hras", "Hro", "Nan", "Khard", "Xu", "Hor", "Hro", "Er", "El", "Ul", "Ala"]
+orc_male_p2 = ["", "", "", "", "", "", "", "", "", "", "", "", "an", "", "", ""]
+orc_male_p3 = ["mond", "ran", "cha", "ra",
+               "sa", "ga", "nal", "lo", "gg", "d", "ka", "da", "dag", "lt", "rag"]
+
+orc_fem_p1 = ["Ge", "Hi", "Okan", "Sha", "Sir", "Treg", "So"]
+orc_fem_p2 = ["ri", "", "", "", "se", "", ""]
+orc_fem_p3 = ["na", "ria", "de", "la", "ne", "ga", "ria"]
+
+goblin_male_p1 = ["Gor", "Gro", "Hem", "Hul", "Nis", "Me", "Krad"]
+goblin_male_p2 = ["", "", "", "", "rog", ""]
+goblin_male_p3 = ["mer", "dan", "mar", "sel", "ga", "ms", "d"]
+
+goblin_fem_p1 = ["Har", "Sol", "So", "Chi", "Vela", "Gryn", "Hul"]
+goblin_fem_p2 = ["", "", "me", "", "tu", "", ""]
+goblin_fem_p3 = ["gia", "ga", "ria", "lia", "la", "ne", "mar"]
+
+dwarf_male_p1 = ["Eta", "Ger", "Bod", "Gan", "Har",
+                 "Tyr", "Wul", "Gor", "Gar", "Tor", "An", "Gun", "Gar", "Mur", "Thul", "El", "Sni", "Byr",
+                 "Dru", "Ber", "Mer", "Ka", "Tu", "Tron", "Rang", "Ar", "Lu", "Ul", "Pey", "Vir"]
+dwarf_male_p2 = ["", "", "", "de", "ga", "", "", "", "- ", "", "ter", "", "", "", "der", "", "", "", "", "",
+                  "ro", "",  "de", "", "", "pen", "", "ra", ""]
+dwarf_male_p3 = ["rik", "mond", "lo", "man", "mar", "gar",
+                 "fred", "dor", "mar", "wyld", "dred", "mann", "khal", "bel", "man", "yd", "dra", "nel", "r",
+                 "wyld", "tyld", "nax", "rik", "val",  "mar", "vi", "dus", "m", "man", "dur"]
+
+dwarf_fem_p1 = ["Bra", "Ar", "Po", "Ham", "Hil",
+                "Sko", "Tor", "Trin", "Aga", "Vara", "Ar", "Hün", "Ri", "Sul", "Mil"]
+dwarf_fem_p2 = ["", "", "ri", "hel", "le", "rin", "be", "", "", "", "", "", "gal", "", ""]
+dwarf_fem_p3 = ["ka", "via", "sia", "da", "va",
+                "da", "nia", "di", "tha", "mak", "via", "wut", "da", "ma", "va"]
+
+halfling_male_p1 = ["Ar", "Brol", "Hen", "Hor", "Vil", "Mel", "Lom"]
+halfling_male_p2 = ["", "", "", "ge", "", ""]
+halfling_male_p3 = ["land", "lo", "der", "man", "go", "kar", "mo"]
+
+halfling_fem_p1 = ["Be", "Don", "Gi", "Lyl", "Mol", "Sa", "Me", "Hul"]
+halfling_fem_p2 = ["lin", "del", "li", "", "", "nar", "dul", ""]
+halfling_fem_p3 = ["da", "la", "na", "la", "lia", "si", "da", "via"]
+
+wolf_male_p1 = ["Ala", "Au", "Ja", "Ke", "Na", "Aba"]
+wolf_male_p2 = ["", "ho", "ba", "", "", ""]
+wolf_male_p3 = ["ne", "ne", "ri", "koa", "ako", "ken"]
+
+wolf_fem_p1 = ["Ala", "Ei", "Gae", "Kea", "La"]
+wolf_fem_p2 = ["", "", "", "", "gi"]
+wolf_fem_p3 = ["ni", "nia", "ta", "wa", "ni"]
+
+# dictionary with races - keys are available races, entries are lists of list with the names.
+# each entry should have 2 lists - 1st with the name part lists for male, 2nd for female
+races_dict = {"Human": [[human_male_p1, human_male_p2, human_male_p3],
+                        [human_fem_p1, human_fem_p2, human_male_p3]],
+              "Dwarf": [[dwarf_male_p1, dwarf_male_p2, dwarf_male_p3],
+                        [dwarf_fem_p1, dwarf_fem_p2, dwarf_fem_p3]],
+              "Elf": [[elf_male_p1, elf_male_p2, elf_male_p3],
+                      [elf_fem_p1, elf_fem_p2, elf_fem_p3]],
+              "Half-Elf": [[halfelf_male_p1, halfelf_male_p2, halfelf_male_p3],
+                           [halfelf_fem_p1, halfelf_fem_p2, halfelf_fem_p3]],
+              "Goblin": [[goblin_male_p1, goblin_male_p2, goblin_male_p3],
+                         [goblin_fem_p1, goblin_fem_p2, goblin_fem_p3]],
+              "Halfling": [[halfling_male_p1, halfling_male_p2, halfling_male_p3],
+                           [halfling_fem_p1, halfling_fem_p2, halfling_fem_p3]],
+              "Orc": [[orc_male_p1, orc_male_p2, orc_male_p3],
+                      [orc_fem_p1, orc_fem_p2, orc_fem_p3]],
+              "Wolfling": [[wolf_male_p1, wolf_male_p2, wolf_male_p3],
+                           [wolf_fem_p1, wolf_fem_p2, wolf_fem_p3]]}
+
+number_names = 100 # currently not used
+
+max_number_retries = 10
+
+save_names = True # currently not used
+
+# ACTUAL SCRIPT
+from random import shuffle
+import easygui as egui
+import csv
+
+# Menu for setting parameters
+race = egui.buttonbox("For what race do you want to generate names?",
+                      "Select race", list(races_dict.keys()))
+gender = egui.buttonbox(
+    "For what gender do you want to generate names?", "Select gender", ["male", "female"])
+selectd_race_name_li = races_dict[race]
+
+if gender == "male":
+    gender_index = 0
+else:
+    gender_index = 1
+
+while True:
+    number_names = egui.enterbox(
+        msg="How many names should be generated?", title="Number Names", default=5)
+
+    try:
+        number_names = int(number_names)
+        break
+    except:
+        egui.msgbox("Please enter a number!")
+
+name_li_p1 = selectd_race_name_li[gender_index][0]
+name_li_p2 = selectd_race_name_li[gender_index][1]
+name_li_p3 = selectd_race_name_li[gender_index][2]
+
+# Test for male humans
+generated_names_li = []
+
+for i in range(number_names):
+    name_ok = False
+    number_retries = 0
+    while name_ok == False:
+        name_ok = True
+
+        shuffle(name_li_p1)
+        shuffle(name_li_p2)
+        shuffle(name_li_p3)
+
+        name = name_li_p1[0] + name_li_p2[0] + name_li_p3[0]
+
+        if number_retries >= max_number_retries:
+            egui.msgbox("Could not find any more valid new names. Stopping name generation at {} names.\n".format(
+                len(generated_names_li)))
+            break
+        elif name in generated_names_li:
+            name_ok = False
+            number_retries += 1
+        else:
+            name_ok = True
+            generated_names_li.append(name)
+
+    if number_retries >= max_number_retries:
+        break
+
+gen_names_str = '\n'.join(generated_names_li)
+gen_names_msg = "Generated Names for {} {}:\n\n{}".format(gender, race, gen_names_str)
+egui.msgbox(gen_names_msg)
+
+save_names = egui.ynbox(msg="Do you want to save the names to a *.csv file?",title = "Save to file?")
+
+if save_names:
+    save_file_name = egui.filesavebox(
+        title="Choose location to save name list", filetypes="*.csv")
+    with open(save_file_name, 'w') as save_file:
+        wr = csv.writer(save_file, delimiter="\n")
+        wr.writerow(generated_names_li)
